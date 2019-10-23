e689bdcfbf8a065dd16f2b360479346485e637b2
meghabs411 <53851853+meghabs411@users.noreply.github.com>
2019-10-23 12:33:53 +0530

    Create 8a.cpp

---
+++ 8a.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int i,N,arr[20],f,ele,s;
+	cout<<"enter the number of elemnts";
+	cin>>N;
+	cout<<"Enter elments";
+	for(int i=1;i<=N;i++)
+	{
+		cin>>arr[i];
+	}
+	for(i=1;i<=N/2;i++)
+	{
+		//cout<<arr[i];
+		f=i;
+		ele=arr[f];
+		s=2*f;
+		if(s<N)
+		{
+			if(arr[s]<ele && arr[s+1]<ele)
+					continue;
+			else
+			{
+				cout<<"False it is not a max heap\n";
+					return 0;
+			}
+		}
+		if(s<=N)
+		{
+			if(arr[s]<ele)
+				continue;
+			else
+			{
+				cout<<"False it is not a  max heap\n";
+				return 0;
+			}
+		}
+	}
+	cout<<"True it is a max\n";
+	return 0;
